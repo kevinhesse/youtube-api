19e1d2b6d8f48e488761a1c318f2af81f984bb45
kevin <khesse92@gmail.com>
2018-07-08 11:16:56 -0400

    created search functionality of youtube API

---
+++ src/components/search_bar.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+//creates a class and extends says give functionality of
+// the funtionality the react.componenet class has
+//every class we create need to be deifined by a render method
+
+class SearchBar extends Component {
+  render() {
+    return <input />;
+  }
+}
+
+
+//changing functonal component below into a class based component
+// const SearchBar = () => {
+//     return <input />;
+// };
+
+export default SearchBar;
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAbjEnlotS8yx3aKjFH7hR-9rfbgEbubuM';
+
 // creating component that creates HTML
 
  const App = () => {
-     return <div>hi!</div>;
+     return(  
+     <div>
+         <SearchBar />
+     </div>
+     )
  }
 
 // take the componemnts and put it on the page
