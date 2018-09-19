d78d9181190021993e8d15ffa0c9b04113f58709
kevin <khesse92@gmail.com>
2018-09-19 05:46:24 -0400

    commit

---
+++ src/components/video_list_item.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+// export const VideoListItem = ({video}) => {
+//     return (
+//         <li className="list-group-item">
+//             <div className="video-list media">
+//               <div className="media-left">
+//                 <img className ="media-object" />
+//               </div>
+//             </div>
+//         </li>
+//     );
+// };
+
