c0fdb82f1ac7d5261e6d23f16b43c6d4fc2b623c
kevin <khesse92@gmail.com>
2018-09-26 15:42:37 -0400

    added div stylings to video list and defined video thumbnail images

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const VideoListItem = (props) => {
-    return <li>Video</li>
+    const videoUrl = video.snippet.thumbnails.default.url;
+    const video = props.video;
+    console.log(video)
+    return (
+    <li className="list-group-item">
+      <div className="video-list media" >
+        <div className="media-left">
+          <img className="media-object" />
+        </div>
+      </div>
+
+      <div className="media-body">
+        <div className="media-heading">
+        </div>
+      </div>
+    </li>
+    )
 };
 
 export default VideoListItem
