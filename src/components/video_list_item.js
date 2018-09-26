5787bd631db098eca4e44cf98c1aab7ffee5effa
kevin <khesse92@gmail.com>
2018-09-26 16:57:05 -0400

    created markup for video_detail

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,21 +1,20 @@
 import React from 'react';
 
 const VideoListItem = (props) => {
+  const video = props.video;
     const videoUrl = video.snippet.thumbnails.default.url;
-    const video = props.video;
     console.log(video)
     return (
     <li className="list-group-item">
       <div className="video-list media" >
         <div className="media-left">
-          <img className="media-object" />
+          <img className="media-object" src={videoUrl} />
         </div>
       </div>
 
       <div className="media-body">
-        <div className="media-heading">
+        <div className="media-heading">{video.snippet.title}</div>
         </div>
-      </div>
     </li>
     )
 };
